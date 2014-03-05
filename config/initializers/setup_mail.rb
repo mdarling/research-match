bdd56aa0e22ca86a6bb032d176e7c1fb3bd862fe
mdarling <michaelcdarling@gmail.com>
2014-03-05 14:33:17 -0700

    minor change

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => "25",
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => ENV['SENDGRID_DOMAIN']
+  :domain         => ENV['SENDGRID_DOMAIN'],
   :authentication       => :plain,
   :enable_starttls_auto => true
 }
