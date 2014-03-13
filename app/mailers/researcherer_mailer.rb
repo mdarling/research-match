422f626f845d7bb09e71fafba7046bd5f5133924
mdarling <michaelcdarling@gmail.com>
2014-03-12 18:28:56 -0600

    m

--- app/mailers/researcherer_mailer.rb
+++ app/mailers/researcherer_mailer.rb
@@ -4,13 +4,13 @@ class ResearchererMailer < ActionMailer::Base
   def matched_students_notification(research_user, student_matches)
    	@student_matches = student_matches
 
-    
-     if project_survey.contact != ""
-     	@contact = project_survey.contact
-     else
-     	@contact = project_survey.researcher
-     end
-     contact = @contact
+    @contact = research_user.first_name + " " + research_user.last_name
+    # if project_survey.contact != ""
+    # 	@contact = project_survey.contact
+    # else
+    # 	@contact = project_survey.researcher
+    # end
+    # contact = @contact
     #email = research_user.email
     email = "amrit914@gmail.com"
     mail(:to => email, :subject => "Students Matched To Your Project")
