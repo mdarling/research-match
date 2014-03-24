87018f9f65aec5a3d39f6cd77b3a93ac9692288b
mdarling <michaelcdarling@gmail.com>
2014-03-24 00:52:35 -0600

    m

--- app/models/project_survey.rb
+++ app/models/project_survey.rb
@@ -3,8 +3,8 @@ class ProjectSurvey < ActiveRecord::Base
 
 	has_many :positions
 	belongs_to :research_user
-	belongs_to :department
+	
 	accepts_nested_attributes_for :positions, allow_destroy: true
 
-	validates :department_id, :email, :description, :title, :keywords, :researcher, presence: true
+	validates :department, :email, :description, :title, :keywords, :researcher, presence: true
 end
