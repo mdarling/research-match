b43a053f8a252dccb57eddd018a41ed7e2d07279
mdarling <michaelcdarling@gmail.com>
2014-04-02 18:48:16 -0600

    m

--- app/mailers/researcherer_mailer.rb
+++ app/mailers/researcherer_mailer.rb
@@ -1,5 +1,5 @@
 class ResearchererMailer < ActionMailer::Base
-  default :from => "research-match@unm.edu"
+  default :from => "researchmatch@unm.edu"
 
   def matched_students_notification(research_user, student_matches)
    	@student_matches = student_matches
@@ -14,6 +14,28 @@ class ResearchererMailer < ActionMailer::Base
     email = research_user.email
     #email = "amrit914@gmail.com"
     mail(:to => email, :subject => "Students Matched To Your Project")
+  end #end matched students notification
+
+  def researcher_no_project
+    researchers = ResearchUser.all
+    
+    
+    researchers.each do |@researcher|
+      if @researcher.project_surveys.empty?
+        email = @researcher.email
+        mail(:to => "amrit914@gmail.com", :subject => "UNM Research Match has Launched!")
+      end
+    end
+  end 
+
+  def researcher_with_project
+    researchers = ResearchUser.all
+
+    researchers.each do |@researcher|
+      unless @researcher.project_surveys.empty?
+        email = @researcher.email
+        mail(:to => "amrit914@gmail.com", :subject => "UNM Research Match has Launched!")
+      end
   end
 
 end
