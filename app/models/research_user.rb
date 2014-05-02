b56f4eb85a52371f5fd8d5861ce4634217fe7d72
mdarling <michaelcdarling@gmail.com>
2014-05-02 14:29:26 -0600

    m

--- app/models/research_user.rb
+++ app/models/research_user.rb
@@ -1,8 +1,8 @@
 class ResearchUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable, :confirmable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :confirmable, :validatable
 
   # attr_accessible :email, :password, :password_confirmation, :remember_me,
   # :first_name, :last_name
