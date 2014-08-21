c08d01393242c5c067140960945ac09a75be880f
mdarling <michaelcdarling@gmail.com>
2014-08-21 10:42:31 +0530

    Procfile

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,23 @@
 # config/unicorn.rb
-if ENV["RAILS_ENV"] == "development"
-  worker_processes 1
-else
-  worker_processes 3
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
 end
 
-timeout 30
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
